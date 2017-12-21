c001d9e77aaec6191f53e335d49c92d86c4f0ef0
Angel Solis <angel.solis@dsindigo.com>
2017-12-21 12:36:43 -0600

    testedgit add .

--- kataCodeSmell/src/test/java/com/gildedrose/GildedRoseTest.java
+++ kataCodeSmell/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -25,4 +25,69 @@ public class GildedRoseTest {
       assertEquals(0, item.sellIn);
       assertEquals("Sulfuras, Hand of Ragnaros", item.name);
     }
+    
+    @Test
+    public void ConjuredTest()
+    {
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        int sellin = 10, quality = 20;
+        for (int i = 0; i < 5; i++)
+        {
+            assertEquals(sellin, app.items[0].sellIn);
+            assertEquals(quality, app.items[0].quality);
+            app.updateQuality();
+            sellin--;
+            quality -= 2;
+            //System.out.println(app.items[0].sellIn + " " + sellin + "----" + app.items[0].quality + " " + quality);
+        }
+    }
+    
+    @Test
+    public void Backstage_Test()
+    {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20) };
+        GildedRose app = new GildedRose(items);
+        int sellin = -1, quality = 20;
+        for (int i = 0; i < 5; i++)
+        {
+            assertEquals(sellin, app.items[0].sellIn);
+            if (i != 0)
+                quality = 0;
+            assertEquals(quality, app.items[0].quality);
+            app.updateQuality();
+            sellin--;
+            //System.out.println(app.items[0].sellIn + " " + sellin + "----" + app.items[0].quality + " " + quality);
+        }
+    }
+    
+    @Test
+    public void aged_Test()
+    {
+        Item[] items = new Item[] { new Item("Aged Brie", 6, 10) };
+        GildedRose app = new GildedRose(items);
+        int sellin = 6, quality = 10;
+        for (int i = 0; i < 5; i++)
+        {
+            assertEquals(sellin, app.items[0].sellIn);
+            assertEquals(quality, app.items[0].quality);
+            app.updateQuality();
+            sellin--;
+            quality++;
+           // System.out.println(app.items[0].sellIn + " " + sellin + "----" + app.items[0].quality + " " + quality);
+        }
+    }
+    
+    @Test
+    public void sulfuras_Test()
+    {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 6, 10) };
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 5; i++)
+        {
+            assertEquals(6, app.items[0].sellIn);
+            assertEquals(10, app.items[0].quality);
+            app.updateQuality();
+        }
+    }
 }
